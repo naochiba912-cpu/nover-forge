84854bffb81ca0940b73376031de2a2a704c1564
Deploy User <deploy@example.com>
2026-06-08 00:33:47 +0900

    Fix Render build by pushing dist directory

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
-
 echo "Installing backend dependencies..."
 cd backend
 pip install -r requirements.txt
-
-echo "Building frontend..."
-cd ../frontend
-npm install
-npm run build
-
 echo "Build complete!"
